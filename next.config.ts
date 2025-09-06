056c3f41e9fab6f86635afc0eea8b1c492e31b76
Developer Sam <developer.sami44@gmail.com>
2025-09-06 19:46:42 +0000

    ⚠ Invalid next.config.ts options detected:  ⚠     Unrecognized key(s) i

--- next.config.ts
+++ next.config.ts
@@ -24,7 +24,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https'
+        protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
         port: '',
         pathname: '/**',
